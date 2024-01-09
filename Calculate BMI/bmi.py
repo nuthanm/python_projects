2c04bac8d0d0aed82c3cbe3f44c7b680777d821e
nuthan <inbox.nuthan@gmail.com>
2024-01-09 17:29:58 +0530

    Added logic and necessary files to test BMI

---
+++ Calculate BMI/bmi.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/calculate', methods=['POST'])
+def calculate():
+    if request.method == 'POST':
+        weight = float(request.form['weight'])
+        height = float(request.form['height'])
+
+        # BMI Calculation
+        bmi = weight / (height * height)
+
+        return render_template('index.html', bmi=bmi)
+
+if __name__ == '__main__':
+    app.run(debug=True)
